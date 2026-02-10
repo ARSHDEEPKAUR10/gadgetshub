48c18e952e5a8ab0d539be2af396fc2a2398c494
Arshdeep Kaur <arshdeepkaur10@academic.rrc.ca>
2026-02-10 10:20:01 -0600

    update homepage file.

--- gadgetshub/src/pages/HomePage.tsx
+++ gadgetshub/src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useMemo, useState } from "react";
-import "./HomePage.css";
+import "../pages/Homepage.css"
 const terms = ["Smartphones", "Laptops", "Headphones", "Accessories"];
 
 export default function HomePage() {
